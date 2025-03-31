39d81f5b11039c7a1e808f553c1a92bc64494fc7
yarintDBmaestro <yarintDBmaestro>
2025-03-31 15:39:40 +0300

    add newTables (2)

---
+++ dbo/Tables/Creation-Scripts/NewTable.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NewTable](
+	[Column1] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column2] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+GO
---
+++ dbo/Tables/Creation-Scripts/NewTable2.sql
@@ -0,0 +1,8 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NewTable2](
+	[Column1] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+GO
