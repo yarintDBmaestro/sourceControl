220fb2ca11290211b98cbaacff2b1f9f7f58a044
yarintDBmaestro <yarintDBmaestro>
2025-04-01 15:13:14 +0300

    initial main

--- dbo/Tables/Creation-Scripts/NewTable.sql
+++ dbo/Tables/Creation-Scripts/NewTable.sql
@@ -3,6 +3,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[NewTable](
-	[column1] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[Column1] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
