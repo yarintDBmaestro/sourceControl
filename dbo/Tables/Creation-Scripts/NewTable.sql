abfc7a1a7d513c976f0e8ebb7cdc84c79a277515
yarintDBmaestro <yarintDBmaestro>
2025-04-01 15:30:38 +0300

    add col3

--- dbo/Tables/Creation-Scripts/NewTable.sql
+++ dbo/Tables/Creation-Scripts/NewTable.sql
@@ -4,6 +4,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[NewTable](
 	[column1] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Column2] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[Column2] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column3] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
