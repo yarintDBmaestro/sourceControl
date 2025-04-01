a9e6ddcf3a842b932def22f03d3dc00de101bc66
yarintDBmaestro <yarintDBmaestro>
2025-04-01 14:45:04 +0300

    initial commit second

--- dbo/Tables/Creation-Scripts/NewTable2.sql
+++ dbo/Tables/Creation-Scripts/NewTable2.sql
@@ -3,8 +3,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[NewTable2](
-	[Column1] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Column2T2] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Column3T2] [varchar](75) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[Column1] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
